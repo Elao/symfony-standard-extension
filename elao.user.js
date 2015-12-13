0a5cda328c0e7ad72dbeecd4470aecdccb151c87
Arnaud Benassy <abenassy@gmail.com>
2015-12-13 17:01:31 +0100

    Improve script to display userscript-bar more discreetly

--- elao.user.js
+++ elao.user.js
@@ -31,21 +31,32 @@
     }
 
     addStyle( [
-        "#userscript-tools-bar { position:fixed; top:0; right:0; background-color:rgba(0, 0, 0, .4); opacity: .2; border-radius : 0 0 0 4px;  transition: opacity 500ms; z-index:9999;}",
-        "#userscript-tools-bar:hover { opacity: 1;}",
-        "#userscript-tools-bar a { display:inline-block; padding:5px; color:white; line-height:20px; font-size:14px; }",
-        "#userscript-tools-bar a:not(:last-child)::after { content : ' | ';}"
+        "#userscript-tools-bar { position:fixed; top:-30px; right:0; background-color:rgba(0, 0, 0, .4); opacity: .2; border-radius : 0 0 0 4px;  transition: all 500ms; z-index:9999;}",
+        "#userscript-tools-bar:hover { opacity: 1; top:0; }",
+        "#userscript-tools-bar > div#userscript-tools-hook { text-align:center; line-height:20px; }",
+        "#userscript-tools-bar > div > a { display:inline-block; padding:5px; color:white; line-height:20px; font-size:14px; }",
+        "#userscript-tools-bar > div > a:not(:last-child)::after { content : ' | ';}"
     ] );
 
     var baseUrl = location.protocol + '//' + document.domain;
     var bar     = document.createElement('div');
 
     bar.setAttribute('id', 'userscript-tools-bar');
-    bar.appendChild(createLink('Supervisor',    baseUrl + ':9001'));
-    bar.appendChild(createLink('Mailcatcher',   baseUrl + ':1080'));
-    bar.appendChild(createLink('Log.io',        baseUrl + ':28778'));
-    bar.appendChild(createLink('OPCache',       baseUrl + ':2013'));
-    bar.appendChild(createLink('DB Admin',      baseUrl + ':1980'));
+
+    var linkContainer = document.createElement('div');
+
+    linkContainer.appendChild(createLink('Supervisor',    baseUrl + ':9001'));
+    linkContainer.appendChild(createLink('Mailcatcher',   baseUrl + ':1080'));
+    linkContainer.appendChild(createLink('Log.io',        baseUrl + ':28778'));
+    linkContainer.appendChild(createLink('OPCache',       baseUrl + ':2013'));
+    linkContainer.appendChild(createLink('PGAdmin',       baseUrl + ':1980'));
+
+    var hookContainer = document.createElement('div');
+    hookContainer.innerHTML = "=";
+    hookContainer.setAttribute('id', 'userscript-tools-hook');
+
+    bar.appendChild(linkContainer);
+    bar.appendChild(hookContainer);
 
     document.body.appendChild(bar);
 
